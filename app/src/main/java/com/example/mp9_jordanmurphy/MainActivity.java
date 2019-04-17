bd2bd599f487bc484420dedbbe36ece0e93b3235
jmurphy8795 <jmurphy8795@tamu.edu>
2019-04-16 23:01:51 -0500

    finished

--- app/src/main/java/com/example/mp9_jordanmurphy/MainActivity.java
+++ app/src/main/java/com/example/mp9_jordanmurphy/MainActivity.java
@@ -1,13 +1,465 @@
 package com.example.mp9_jordanmurphy;
 
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.common.FirebaseMLException;
+import com.google.firebase.ml.custom.FirebaseModelDataType;
+import com.google.firebase.ml.custom.FirebaseModelInputOutputOptions;
+import com.google.firebase.ml.custom.FirebaseModelInputs;
+import com.google.firebase.ml.custom.FirebaseModelInterpreter;
+import com.google.firebase.ml.custom.FirebaseModelManager;
+import com.google.firebase.ml.custom.FirebaseModelOptions;
+import com.google.firebase.ml.custom.FirebaseModelOutputs;
+import com.google.firebase.ml.custom.model.FirebaseCloudModelSource;
+import com.google.firebase.ml.custom.model.FirebaseLocalModelSource;
+import com.google.firebase.ml.custom.model.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.example.mp9_jordanmurphy.GraphicOverlay.Graphic;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "MainActivity";
+    private static final int REQ_CODE_TAKE_PICTURE = 123;
+    private Button takeImage, mTextButton, labelButton;
+    private ImageView camera_image;
+    private GraphicOverlay mGraphicOverlay;
+    private Bitmap mSelectedImage;
+    private Bitmap bmp;
+    private ListView list;
+    private ArrayAdapter<String> listAdapter ;
+    // Max width (portrait mode)
+    private Integer mImageMaxWidth;
+    // Max height (portrait mode)
+    private Integer mImageMaxHeight;
+
+    private static final String LABEL_PATH = "labels.txt";
+    /**
+     * Number of results to show in the UI.
+     */
+    private static final int RESULTS_TO_SHOW = 3;
+    /**
+     * Dimensions of inputs.
+     */
+    private static final int DIM_BATCH_SIZE = 1;
+    private static final int DIM_PIXEL_SIZE = 3;
+    private static final int DIM_IMG_SIZE_X = 224;
+    private static final int DIM_IMG_SIZE_Y = 224;
+    /**
+     * Labels corresponding to the output of the vision model.
+     */
+    private List<String> mLabelList;
+
+    private final PriorityQueue<Map.Entry<String, Float>> sortedLabels =
+            new PriorityQueue<>(
+                    RESULTS_TO_SHOW,
+                    new Comparator<Map.Entry<String, Float>>() {
+                        @Override
+                        public int compare(Map.Entry<String, Float> o1, Map.Entry<String, Float>
+                                o2) {
+                            return (o1.getValue()).compareTo(o2.getValue());
+                        }
+                    });
+    /* Preallocated buffers for storing image data. */
+    private final int[] intValues = new int[DIM_IMG_SIZE_X * DIM_IMG_SIZE_Y];
+
+    /**
+     * An instance of the driver class to run model inference with Firebase.
+     */
+    private FirebaseModelInterpreter mInterpreter;
+    /**
+     * Data configuration of input & output data of model.
+     */
+    private FirebaseModelInputOutputOptions mDataOptions;
+    private static final String HOSTED_MODEL_NAME = "cloud_model_1";
+    private static final String LOCAL_MODEL_ASSET = "mobilenet_v1_1.0_224_quant.tflite";
+    //private static final String LOCAL_MODEL_ASSET = "optimized_graph.lite";
+
+    //ArrayList<String> wordList = new ArrayList<String>();
+    //ArrayAdapter<String> adapter;
+    private ArrayAdapter<String> arrayAdapter;
+    private List<String> wordList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        takeImage = findViewById(R.id.image);
+        camera_image = findViewById(R.id.camera_image);
+        mGraphicOverlay = findViewById(R.id.graphic_overlay);
+        mTextButton = findViewById(R.id.recognition);
+        list = findViewById(R.id.list);
+        labelButton = findViewById(R.id.label);
+
+        //adapter = new ArrayAdapter<String>(this, R.layout.activity_main, wordList);
+        wordList = new ArrayList<String>();
+
+        // Create an ArrayAdapter from List
+        arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, wordList);
+
+        // DataBind ListView with items from ArrayAdapter
+        list.setAdapter(arrayAdapter);
+
+        takeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent picIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(picIntent, REQ_CODE_TAKE_PICTURE);
+            }
+        });
+        mTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (mSelectedImage != null) {
+                    Log.v("selected image", "string msg");
+                    // Get the dimensions of the View
+                    Pair<Integer, Integer> targetedSize = getTargetedWidthHeight();
+
+                    int targetWidth = targetedSize.first;
+                    int maxHeight = targetedSize.second;
+
+                    // Determine how much to scale down the image
+                    float scaleFactor =
+                            Math.max(
+                                    (float) mSelectedImage.getWidth() / (float) targetWidth,
+                                    (float) mSelectedImage.getHeight() / (float) maxHeight);
+
+                    Bitmap resizedBitmap =
+                            Bitmap.createScaledBitmap(
+                                    mSelectedImage,
+                                    (int) (mSelectedImage.getWidth() / scaleFactor),
+                                    (int) (mSelectedImage.getHeight() / scaleFactor),
+                                    true);
+
+                    camera_image.setImageBitmap(resizedBitmap);
+                    mSelectedImage = resizedBitmap;
+                }
+                runTextRecognition();
+            }
+        });
+
+        labelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                runModelInference();
+
+            }
+        });
+
+        initCustomModel();
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent intent) {
+        if (requestCode ==REQ_CODE_TAKE_PICTURE
+                && resultCode == RESULT_OK)
+        {
+            bmp = (Bitmap)intent.getExtras().get("data");
+            camera_image.setImageBitmap(bmp);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bmp.compress(Bitmap.CompressFormat.PNG, 0, stream);
+            mGraphicOverlay.clear();
+            mSelectedImage = bmp;
+
+            //runTextRecognition();
+        }
+    }
+
+    private void runTextRecognition() {
+        // Replace with code from the codelab to run text recognition.
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(mSelectedImage);
+        FirebaseVisionTextRecognizer recognizer = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
+        mTextButton.setEnabled(false);
+        recognizer.processImage(image)
+                .addOnSuccessListener(
+                        new OnSuccessListener<FirebaseVisionText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionText texts) {
+                                mTextButton.setEnabled(true);
+                                processTextRecognitionResult(texts);
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                mTextButton.setEnabled(true);
+                                e.printStackTrace();
+                            }
+                        });
+    }
+
+    private void processTextRecognitionResult(FirebaseVisionText texts) {
+        // Replace with code from the codelab to process the text recognition result.
+        List<FirebaseVisionText.TextBlock> blocks = texts.getTextBlocks();
+        if (blocks.size() == 0) {
+            showToast("No text found");
+            return;
+        }
+        mGraphicOverlay.clear();
+        for (int i = 0; i < blocks.size(); i++) {
+            List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
+            for (int j = 0; j < lines.size(); j++) {
+                List<FirebaseVisionText.Element> elements = lines.get(j).getElements();
+                for (int k = 0; k < elements.size(); k++) {
+                    Graphic textGraphic = new TextGraphic(mGraphicOverlay, elements.get(k));
+                    mGraphicOverlay.add(textGraphic);
+                    showToast(elements.get(k).getText());
+                    wordList.add(elements.get(k).getText());
+                    //wordList.add("hello");
+                }
+            }
+        }
+        arrayAdapter.notifyDataSetChanged();
+    }
+
+    private synchronized List<String> getTopLabels(byte[][] labelProbArray) {
+        for (int i = 0; i < mLabelList.size(); ++i) {
+            sortedLabels.add(
+                    new AbstractMap.SimpleEntry<>(mLabelList.get(i), (labelProbArray[0][i] &
+                            0xff) / 255.0f));
+            if (sortedLabels.size() > RESULTS_TO_SHOW) {
+                sortedLabels.poll();
+            }
+        }
+        List<String> result = new ArrayList<>();
+        final int size = sortedLabels.size();
+        for (int i = 0; i < size; ++i) {
+            Map.Entry<String, Float> label = sortedLabels.poll();
+            result.add(label.getKey() + ":" + label.getValue());
+        }
+
+        Log.d(TAG, "labels: " + result.toString());
+        return result;
+    }
+
+    /**
+     * Reads label list from Assets.
+     */
+    private List<String> loadLabelList(Activity activity) {
+        List<String> labelList = new ArrayList<>();
+        try (BufferedReader reader =
+                     new BufferedReader(new InputStreamReader(activity.getAssets().open
+                             (LABEL_PATH)))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                labelList.add(line);
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to read label list.", e);
+        }
+        return labelList;
+    }
+
+    /**
+     * Writes Image data into a {@code ByteBuffer}.
+     */
+    private synchronized ByteBuffer convertBitmapToByteBuffer(
+            Bitmap bitmap, int width, int height) {
+        ByteBuffer imgData =
+                ByteBuffer.allocateDirect(
+                        DIM_BATCH_SIZE * DIM_IMG_SIZE_X * DIM_IMG_SIZE_Y * DIM_PIXEL_SIZE);
+        imgData.order(ByteOrder.nativeOrder());
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, DIM_IMG_SIZE_X, DIM_IMG_SIZE_Y,
+                true);
+        imgData.rewind();
+        scaledBitmap.getPixels(intValues, 0, scaledBitmap.getWidth(), 0, 0,
+                scaledBitmap.getWidth(), scaledBitmap.getHeight());
+        // Convert the image to int points.
+        int pixel = 0;
+        for (int i = 0; i < DIM_IMG_SIZE_X; ++i) {
+            for (int j = 0; j < DIM_IMG_SIZE_Y; ++j) {
+                final int val = intValues[pixel++];
+                imgData.put((byte) ((val >> 16) & 0xFF));
+                imgData.put((byte) ((val >> 8) & 0xFF));
+                imgData.put((byte) (val & 0xFF));
+            }
+        }
+        return imgData;
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    // Functions for loading images from app assets.
+
+    // Returns max image width, always for portrait mode. Caller needs to swap width / height for
+    // landscape mode.
+    private Integer getImageMaxWidth() {
+        if (mImageMaxWidth == null) {
+            // Calculate the max width in portrait mode. This is done lazily since we need to
+            // wait for
+            // a UI layout pass to get the right values. So delay it to first time image
+            // rendering time.
+            mImageMaxWidth = camera_image.getWidth();
+        }
+
+        return mImageMaxWidth;
+    }
+
+    // Returns max image height, always for portrait mode. Caller needs to swap width / height for
+    // landscape mode.
+    private Integer getImageMaxHeight() {
+        if (mImageMaxHeight == null) {
+            // Calculate the max width in portrait mode. This is done lazily since we need to
+            // wait for
+            // a UI layout pass to get the right values. So delay it to first time image
+            // rendering time.
+            mImageMaxHeight =
+                    camera_image.getHeight();
+        }
+
+        return mImageMaxHeight;
+    }
+
+    // Gets the targeted width / height.
+    private Pair<Integer, Integer> getTargetedWidthHeight() {
+        int targetWidth;
+        int targetHeight;
+        int maxWidthForPortraitMode = getImageMaxWidth();
+        int maxHeightForPortraitMode = getImageMaxHeight();
+        targetWidth = maxWidthForPortraitMode;
+        targetHeight = maxHeightForPortraitMode;
+        return new Pair<>(targetWidth, targetHeight);
+    }
+
+    private void initCustomModel() {
+        mLabelList = loadLabelList(this);
+
+        int[] inputDims = {DIM_BATCH_SIZE, DIM_IMG_SIZE_X, DIM_IMG_SIZE_Y, DIM_PIXEL_SIZE};
+        int[] outputDims = {DIM_BATCH_SIZE, mLabelList.size()};
+        try {
+            mDataOptions =
+                    new FirebaseModelInputOutputOptions.Builder()
+                            .setInputFormat(0, FirebaseModelDataType.BYTE, inputDims)
+                            .setOutputFormat(0, FirebaseModelDataType.BYTE, outputDims)
+                            .build();
+            FirebaseModelDownloadConditions conditions = new FirebaseModelDownloadConditions
+                    .Builder()
+                    .requireWifi()
+                    .build();
+            FirebaseLocalModelSource localSource =
+                    new FirebaseLocalModelSource.Builder("asset")
+                            .setAssetFilePath(LOCAL_MODEL_ASSET).build();
+
+            FirebaseCloudModelSource cloudSource = new FirebaseCloudModelSource.Builder
+                    (HOSTED_MODEL_NAME)
+                    .enableModelUpdates(true)
+                    .setInitialDownloadConditions(conditions)
+                    .setUpdatesDownloadConditions(conditions)  // You could also specify
+                    // different conditions
+                    // for updates
+                    .build();
+            FirebaseModelManager manager = FirebaseModelManager.getInstance();
+            manager.registerLocalModelSource(localSource);
+            manager.registerCloudModelSource(cloudSource);
+            FirebaseModelOptions modelOptions =
+                    new FirebaseModelOptions.Builder()
+                            .setCloudModelName(HOSTED_MODEL_NAME)
+                            .setLocalModelName("asset")
+                            .build();
+            mInterpreter = FirebaseModelInterpreter.getInstance(modelOptions);
+        } catch (FirebaseMLException e) {
+            showToast("Error while setting up the model");
+            e.printStackTrace();
+        }
+    }
+
+    private void runModelInference() {
+        if (mInterpreter == null) {
+            Log.e(TAG, "Image classifier has not been initialized; Skipped.");
+            return;
+        }
+        // Create input data.
+        ByteBuffer imgData = convertBitmapToByteBuffer(mSelectedImage, mSelectedImage.getWidth(),
+                mSelectedImage.getHeight());
+
+        try {
+            FirebaseModelInputs inputs = new FirebaseModelInputs.Builder().add(imgData).build();
+            // Here's where the magic happens!!
+            mInterpreter
+                    .run(inputs, mDataOptions)
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            e.printStackTrace();
+                            showToast("Error running model inference");
+                        }
+                    })
+                    .continueWith(
+                            new Continuation<FirebaseModelOutputs, List<String>>() {
+                                @Override
+                                public List<String> then(Task<FirebaseModelOutputs> task) {
+                                    byte[][] labelProbArray = task.getResult()
+                                            .<byte[][]>getOutput(0);
+                                    List<String> topLabels = getTopLabels(labelProbArray);
+                                    //wordList = (ArrayList) topLabels;
+                                    //wordList.add(topLabels);
+                                    //arrayAdapter.notifyDataSetChanged();
+                                    mGraphicOverlay.clear();
+                                    GraphicOverlay.Graphic labelGraphic = new LabelGraphic
+                                            (mGraphicOverlay, topLabels);
+                                    mGraphicOverlay.add(labelGraphic);
+                                    return topLabels;
+                                }
+                            });
+        } catch (FirebaseMLException e) {
+            e.printStackTrace();
+            showToast("Error running model inference");
+        }
+
     }
 }
+
+
+
+
